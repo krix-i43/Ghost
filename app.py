a748d66440a01b1bbd1992a92f1859025faac292
krix-i43 <muthumk1334@gmail.com>
2026-01-01 12:02:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -853,5 +853,5 @@ if __name__ == "__main__":
         time.sleep(3)
 
     # 🌍 Render dynamic port
-    port = int(os.environ.get("PORT", 15029))
+    port = int(os.environ.get("PORT", 15085))
     app.run(host="0.0.0.0", port=port, debug=False)
